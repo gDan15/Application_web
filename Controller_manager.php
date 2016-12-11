9971beb4b3a22c3bff96010e0cf38f1d061be2ed
gaetan guru <gaetan guru>
2016-12-11 16:57:28 +0100

    Fifth_page.php

--- Controller_manager.php
+++ Controller_manager.php
@@ -2,29 +2,27 @@
 // Connexion à la DB
 try
 {
-  $bdd = new PDO('mysql:host=localhost;dbname=jeux_video;charset=utf8','root','');
+  $bdd = new PDO('mysql:host=localhost;dbname=test;charset=utf8','root','');
   //echo 'est rentre dans le try';
 }
 catch(Exception $e)
 {
   die('Erreur : '.$e->getMessage());
 }
-$reponse=$bdd->query('SELECT * FROM jeux_video');
+include 'Fifth_page.php';
+// $reponse=$bdd->query('SELECT * FROM jeux_video');
+
 // On ajoute une entrée dans la table jeux_video
 // $bdd->exec('INSERT INTO jeux_video(nom, possesseur, console, prix, nbre_joueurs_max, commentaires)
 // VALUES(\'Battlefield 1942\', \'Patrick\', \'PC\', 45, 50, \'2nde guerre mondiale\')');
+
 //permet de supprimer toutes les entrées dont les noms sont égos à Battlefield 1942
-$bdd->exec('DELETE FROM jeux_video WHERE nom=\'Dead\'');
+// $bdd->exec('DELETE FROM jeux_video WHERE nom=\'Dead\'');
 
 
-while($donnees = $reponse->fetch())
-{
-?>
-  <p>
-  <strong> <?php echo $donnees['nom'];  ?></strong> <br/>
-  </p>
-<?php
-}
+// $donnees = $reponse->fetch()
+// <strong> echo $donnees['nom'];  </strong> <br/>
+
 // // Affichage des données
 // $reponse = $bdd->query('SELECT lastname FROM users');
 // while ($donnees = $reponse->fetch())
@@ -36,5 +34,5 @@ while($donnees = $reponse->fetch())
 // $bdd->exec(" INSERT INTO users SET lastname='Michel', firstname='Charles', email='cm@ecam.be', mobile='0478/123456' ) ;");
 // // Modification d'un utilisateur$
 // //$nb_modifs = $bdd->exec(" UPDATE users SET email = 'c.michel@ecam.be' WHERE lastname = 'Michel' ");
-$reponse->closeCursor(); // Termine le traitement de la requête
+// $reponse->closeCursor(); // Termine le traitement de la requête
 ?>
---
+++ Fifth_page.php
@@ -0,0 +1,27 @@
+<html>
+  <head>
+    <title> Reservation -- Détails </title>
+    <link rel="stylesheet" type="text/css" href="Style.css">
+  </head>
+  <body>
+    <form method="POST" action="Controller_manager.php">
+      <title>Liste des réservations</title>
+      <table class="table1">
+        <tr>
+          <td> Destination </td>
+          <td> Assurance </td>
+          <td> Total </td>
+          <td> Nom - Age </td>
+        </tr>
+        <!-- <?php
+          $reponse=$bdd->query('SELECT * FROM jeux_video');
+          while($donnees = $reponse->fetch()){
+        ?> -->
+
+        <!-- <?php
+          }
+        ?> -->
+      </table1>
+    </form>
+  </body>
+</html>
